7a656f285ab6a4ce06cacbf4e9bdcdf70ddbf73f
Gurkan <hello@gurkz.me>
2025-09-21 19:27:26 +0200

    separate device selector into its own component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,77 +1,15 @@
-import { createEffect, createSignal, onMount } from "solid-js";
-import { invoke } from "@tauri-apps/api/core";
-import { load } from "@tauri-apps/plugin-store";
-import { appDataDir } from "@tauri-apps/api/path";
 import { SongList } from "@/components/song-list";
-import { selectedDevice, setSelectedDevice } from "@/libs/device";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { StatusButtons } from "@/components/status-buttons";
 import { SongAdder } from "@/components/song-adder";
-
-const store = await load("store.json");
-
-const [devices, setDevices] = createSignal<string[]>([]);
+import { DeviceSelector } from "@/components/device-selector";
 
 export function HomePage() {
-  onMount(async () => {
-    console.log(await appDataDir());
-
-    const output = await store.get("speaker-output");
-
-    if (typeof output === "string") {
-      setSelectedDevice(output);
-    }
-
-    const deviceList = await invoke<string[]>("list_audio_devices");
-    setDevices(deviceList);
-
-    if (deviceList.length > 0 && !output) {
-      setSelectedDevice(deviceList[0]);
-    }
-  });
-
-  // Synchronize the selectedDevice signal with the store
-  createEffect(async () => {
-    const currentDevice = selectedDevice();
-    if (currentDevice) {
-      await store.set("speaker-output", currentDevice);
-    }
-  });
-
   return (
     <>
       <SongAdder />
-      {devices().length > 0 && (
-        <>
-          <Select
-            required
-            onChange={setSelectedDevice}
-            value={selectedDevice()}
-            options={devices()}
-            itemComponent={(props) => (
-              <SelectItem item={props.item}>{props.item.rawValue}</SelectItem>
-            )}
-          >
-            <SelectTrigger>
-              <SelectValue<string>>
-                {(state) => state.selectedOption()}
-              </SelectValue>
-            </SelectTrigger>
-            <SelectContent />
-          </Select>
-        </>
-      )}
-
+      <DeviceSelector />
       <SongList />
-
       <br />
-
       <StatusButtons />
     </>
   );
