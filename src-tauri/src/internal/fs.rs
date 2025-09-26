69f52dec3ff3bab200a1be511bffd8bb2555dbf4
Gurkan <hello@gurkz.me>
2025-09-26 15:13:20 +0200

    use a stream for download_file
    this should speed it up

--- src-tauri/src/internal/fs.rs
+++ src-tauri/src/internal/fs.rs
@@ -1,17 +1,21 @@
 use anyhow::Result;
+use futures_util::StreamExt; // for .next()
 use std::path::Path;
 use tauri_plugin_http::reqwest;
 use tokio::fs::File;
-use tokio::io::AsyncWriteExt; // for write_all
+use tokio::io::AsyncWriteExt;
 
 pub async fn download_file(url: &str, dest_path: &Path) -> Result<()> {
     let response = reqwest::get(url).await?.error_for_status()?;
 
-    let bytes = response.bytes().await?;
-
     let mut file = File::create(dest_path).await?;
 
-    file.write_all(&bytes).await?;
+    let mut stream = response.bytes_stream();
+
+    while let Some(chunk) = stream.next().await {
+        let data = chunk?;
+        file.write_all(&data).await?;
+    }
 
     Ok(())
 }
